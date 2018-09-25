f61564a5fe7d3fed7c4507ebb1e4bd286078d47a
Sam Holeman <sam.holeman@token.io>
2018-09-25 14:40:26 -0700

    RD-1073: Populate realm for affiliated TPPs

--- build_proto.rb
+++ build_proto.rb
@@ -1,8 +1,8 @@
 #
 # Fetches specified proto files from the artifact repository.
 #
-TOKEN_PROTOS_VER = "1.1.0"
-RPC_PROTOS_VER = "1.0.124"
+TOKEN_PROTOS_VER = "1.1.16"
+RPC_PROTOS_VER = "1.1.0"
 
 require 'open-uri'
 
