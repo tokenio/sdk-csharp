2973f89264ccb6e65705752ba57d30e2ff422af4
Gianluca Pane <31416110+gianluca-token@users.noreply.github.com>
2019-04-05 16:39:22 -0700

    RD-2314: add GetTokensSample (#34)

---
+++ samples/GetTokensSample.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Tokenio;
+using Tokenio.Proto.Common.TokenProtos;
+
+namespace samples
+{
+    public class GetTokensSample
+    {
+        /// <summary>
+        /// Illustrate Member.GetToken
+        /// </summary>
+        /// <param name="member">Token member</param>
+        public static Token GetToken(Member member, String tokenId)
+        {
+            Token token = member.GetTokenBlocking(tokenId);
+            
+            // get token payload
+            TokenPayload payload = token.Payload;
+            
+            // get signatures
+            IList<TokenSignature> signatures = token.PayloadSignatures;
+
+            return token;
+        }
+
+        /// <summary>
+        /// Illustrate Member.GetTransferTokens
+        /// </summary>
+        /// <param name="member">Token member</param>
+        /// <returns></returns>
+        public static PagedList<Token> GetTransferTokens(Member member)
+        {
+            // last 10 tokens and offset that can be used to get the next 10
+            PagedList<Token> pagedList = member.GetTransferTokensBlocking("", 10);
+
+            return pagedList;
+        }
+
+        /// <summary>
+        /// Illustrate Member.GetAccessTokens
+        /// </summary>
+        /// <param name="member">Token member</param>
+        /// <returns></returns>
+        public static PagedList<Token> GetAccessTokens(Member member)
+        {
+            // last 10 tokens and offset that can be used to get the next 10
+            PagedList<Token> pagedList = member.GetAccessTokensBlocking("", 10);
+
+            return pagedList;
+        }
+    }
+}
