2ef93b1ff2cd828b42db0069a94043407fe12bde
Yuhan Niu <yuhan.niu@token.io>
2019-05-08 15:47:12 -0700

    more

---
+++ tests/CollectionAssert.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Test
+{
+    public static class CollectionAssert
+    {
+        public static void Equivalent<T>(IEnumerable<T> c1, IEnumerable<T> c2)
+        {
+            var d1 = ToDictionary(c1);
+            var d2 = ToDictionary(c2);
+            Assert.Equal(d1, d2);
+        }
+
+        private static IDictionary<T, int> ToDictionary<T>(IEnumerable<T> collection)
+        {
+            var dictionary = new Dictionary<T, int>();
+            foreach (var item in collection)
+            {
+                if (dictionary.ContainsKey(item))
+                {
+                    dictionary[item]++;
+                }
+                else
+                {
+                    dictionary[item] = 1;
+                }
+            }
+
+            return dictionary;
+        }
+    }
+}
