b58ab871a3d5517ea78e73e4ee892eeb1f35c32d
Bhavya Jain <bhavya@appperfect.com>
2019-11-04 04:38:49 -0800

    Add setSource to TransferTokenBuilder

--- core/src/TokenRequests/TokenRequest.cs
+++ core/src/TokenRequests/TokenRequest.cs
@@ -445,6 +445,17 @@ namespace Tokenio.TokenRequests {
             }
 
             /// <summary>
+            /// Optional. Sets the source account to bypass account selection. May be required for
+            /// some banks.
+            /// </summary>
+            /// <param name="source">Source</param>
+            /// <returns>builder</returns>
+            public TransferBuilder SetSource(TransferEndpoint source) {
+                this.requestPayload.TransferBody.Instructions.Source = source;
+                return this;
+            }
+
+            /// <summary>
             /// Optional. Sets the ultimate party to which the money is due.
             /// </summary>
             /// <param name="ultimateCreditor">the ultimate creditor</param>
