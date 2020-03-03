7a19693177b4366580c2edcc3f7a86ec8d749104
Anna Nikulova <anna.nikulova@token.io>
2020-03-02 16:15:54 -0800

    add tests for core

---
+++ core-tests/Security/InMemoryKeyStoreTest.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using Xunit;
+using Tokenio.Security;
+using Tokenio.Utils;
+using static Tokenio.Proto.Common.SecurityProtos.Key.Types.Level;
+using KeyPair = Tokenio.Security.KeyPair;
+
+namespace Test.Security
+{
+    public class InMemoryKeyStoreTest
+    {
+        private IKeyStore keyStore;
+        private string memberId;
+
+        public InMemoryKeyStoreTest()
+        {
+            keyStore = new InMemoryKeyStore();
+            memberId = Util.Nonce();
+        }
+
+        [Fact]
+        public void GetByLevel()
+        {
+            var privileged = TestUtil.GenerateKeyPair(Privileged);
+            var standard = TestUtil.GenerateKeyPair(Standard);
+            var low = TestUtil.GenerateKeyPair(Low);
+            keyStore.Put(memberId, privileged);
+            keyStore.Put(memberId, standard);
+            keyStore.Put(memberId, low);
+            Assert.Equal(privileged, keyStore.GetByLevel(memberId, Privileged));
+            Assert.Equal(standard, keyStore.GetByLevel(memberId, Standard));
+            Assert.Equal(low, keyStore.GetByLevel(memberId, Low));
+        }
+
+        [Fact]
+        public void GetById()
+        {
+            var key1 = TestUtil.GenerateKeyPair(Privileged);
+            var key2 = TestUtil.GenerateKeyPair(Privileged);
+            keyStore.Put(memberId, key1);
+            keyStore.Put(memberId, key2);
+            Assert.Equal(key1, keyStore.GetById(memberId, key1.Id));
+            Assert.Equal(key2, keyStore.GetById(memberId, key2.Id));
+        }
+
+        [Fact]
+        public void KeyList()
+        {
+            var privileged = TestUtil.GenerateKeyPair(Privileged);
+            var standard = TestUtil.GenerateKeyPair(Standard);
+            var low = TestUtil.GenerateKeyPair(Low);
+            keyStore.Put(memberId, privileged);
+            keyStore.Put(memberId, standard);
+            keyStore.Put(memberId, low);
+            var keyList = new List<KeyPair> {privileged, standard, low};
+            CollectionAssert.Equivalent(keyStore.KeyList(memberId), keyList);
+        }
+
+        [Fact]
+        public void DifferentMember()
+        {
+            var member2 = Util.Nonce();
+            var key1 = TestUtil.GenerateKeyPair(Privileged);
+            var key2 = TestUtil.GenerateKeyPair(Privileged);
+            keyStore.Put(memberId, key1);
+            keyStore.Put(member2, key2);
+            Assert.Equal(key1, keyStore.GetById(memberId, key1.Id));
+            Assert.Equal(key2, keyStore.GetById(member2, key2.Id));
+        }
+    }
+}
---
+++ core-tests/UtilityTest.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using Xunit;
+using Tokenio;
+using Tokenio.Proto.Common.AliasProtos;
+using Tokenio.Proto.Common.TokenProtos;
+using Tokenio.Security;
+using Tokenio.Utils;
+using static Tokenio.Proto.Common.AliasProtos.Alias.Types.Type;
+using static Tokenio.Proto.Common.TokenProtos.AccessBody.Types;
+using static Tokenio.Proto.Common.TokenProtos.AccessBody.Types.Resource.Types;
+
+namespace Test
+{
+    public class UtilityTest
+    {
+        [Fact]
+        public void JsonSerializer()
+        {
+            var payload = new TokenPayload
+            {
+                To = new TokenMember
+                {
+                    Id = "memberId"
+                },
+                Access = new AccessBody
+                {
+                    Resources =
+                    {
+                        new Resource
+                        {
+                            AllAddresses = new AllAddresses()
+                        }
+                    }
+                },
+                RefId = "refId"
+            };
+            var expected = "{\"access\":{\"resources\":[{\"allAddresses\":{}}]},\"refId\":\"refId\",\"to\":{\"id\":\"memberId\"}}";
+            Assert.Equal(expected, Util.ToJson(payload));
+        }
+
+        [Fact]
+        public void HashAlias()
+        {
+            var alias = new Alias
+            {
+                Type = Email,
+                Value = "bob@token.io"
+            };
+            Assert.Equal("HHzc3XVck27qD2gadGVzjffaBZrU8ZLEd2jmtcyPKeev", Util.NormalizeAndHashAlias(alias));
+        }
+
+        [Fact]
+        public void Base58Hashing()
+        {
+            var result = Base58.Encode(Encoding.UTF8.GetBytes("bob@token.io"));
+            Assert.Equal("2rjpGWoxbc8ASyDVx", result);
+        }
+    }
+}
