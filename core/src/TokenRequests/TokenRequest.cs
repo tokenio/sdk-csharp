64a2b7754abc28eaf44cf21da17336a88b2bd071
Mahendra <Mahendra@appperfect.com>
2020-05-08 11:05:30 +0530

    bump version and nit

--- core/src/TokenRequests/TokenRequest.cs
+++ core/src/TokenRequests/TokenRequest.cs
@@ -495,18 +495,6 @@ namespace Tokenio.TokenRequests
                 return this;
             }
 
-            /// <summary>
-            /// Optional. Sets the source account to bypass account selection. May be required for
-            /// some banks.
-            /// </summary>
-            /// <param name="source">Source</param>
-            /// <returns>builder</returns>
-            public TransferBuilder SetSource(TransferEndpoint source)
-            {
-                this.requestPayload.TransferBody.Instructions.Source = source;
-                return this;
-            }
-            
             /// Optional. In the scenario where TPP wishes to know the user's selection of country and
             /// bank, TPP should provide this url so that Token can make a call with relevant
             /// information as parameters. TPP can use that information to set transfer destination.
